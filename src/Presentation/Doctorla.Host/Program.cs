6d4d4ab4e98d191d5b1bd725ded46a23ec268406
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 15:57:13 +0300

    Update Program.cs

--- src/Presentation/Doctorla.Host/Program.cs
+++ src/Presentation/Doctorla.Host/Program.cs
@@ -1,21 +1,43 @@
 using Doctorla.Host;
 using Doctorla.Host.Configurations;
-using ServiceStack;
+using Doctorla.Infrastructure.Common;
+using FluentValidation.AspNetCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.AddConfigurations();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+StaticLogger.EnsureInitialized();
+Log.Information("Server Booting Up...");
+try
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseHttpsRedirection();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.AddConfigurations();
+    builder.Host.UseSerilog((_, config) =>
+    {
+        config.WriteTo.Console()
+        .ReadFrom.Configuration(builder.Configuration);
+    });
+
+    builder.Services.AddControllers().AddFluentValidation();
+    //builder.Services.AddInfrastructure(builder.Configuration);
+    //builder.Services.AddApplication();
+
+    var app = builder.Build();
+
+    //await app.Services.InitializeDatabasesAsync();
+
+    //app.UseInfrastructure(builder.Configuration);
+    //app.MapEndpoints();
+    app.Run();
+
 }
-
-app.UseServiceStack(new AppHost());
-
-app.Run();
+catch (Exception ex) when (!ex.GetType().Name.Equals("StopTheHostException", StringComparison.Ordinal))
+{
+    StaticLogger.EnsureInitialized();
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    StaticLogger.EnsureInitialized();
+    Log.Information("Server Shutting down...");
+    Log.CloseAndFlush();
+}
