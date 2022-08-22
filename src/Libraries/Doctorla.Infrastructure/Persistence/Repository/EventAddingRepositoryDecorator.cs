9341cc2de63f3ee9fef4e9d2a87d6fbc1c9b63fb
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 19:23:11 +0300

    Create EventAddingRepositoryDecorator.cs

---
+++ src/Libraries/Doctorla.Infrastructure/Persistence/Repository/EventAddingRepositoryDecorator.cs
@@ -0,0 +1,64 @@
+﻿using Doctorla.Application.Common.Persistence;
+using Doctorla.Domain.Common.Contracts;
+
+namespace Doctorla.Infrastructure.Persistence.Repository;
+
+/// <summary>
+/// The repository that implements IRepositoryWithEvents.
+/// Implemented as a decorator. It only augments the Add,
+/// Update and Delete calls where it adds the respective
+/// EntityCreated, EntityUpdated or EntityDeleted event
+/// before delegating to the decorated repository.
+/// </summary>
+public class EventAddingRepositoryDecorator<T> : IRepositoryWithEvents<T>
+    where T : class, IAggregateRoot
+{
+    private readonly IRepository<T> _decorated;
+
+    public EventAddingRepositoryDecorator(IRepository<T> decorated) => _decorated = decorated;
+
+    public Task<T> AddAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> AnyAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> CountAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> GetByIdAsync<TId>(TId id, CancellationToken cancellationToken = default) where TId : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<T>> ListAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
