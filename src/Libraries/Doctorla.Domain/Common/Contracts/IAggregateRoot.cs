056d4b47682b994f528f43c344aed000be297792
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 17:00:27 +0300

    Create IAggregateRoot.cs

---
+++ src/Libraries/Doctorla.Domain/Common/Contracts/IAggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctorla.Domain.Common.Contracts
+{
+    // Apply this marker interface only to aggregate root entities
+    // Repositories will only work with aggregate roots, not their children
+    public interface IAggregateRoot : IEntity
+    {
+    }
+}
