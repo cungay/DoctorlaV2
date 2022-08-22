5159cb8247c7071c15ff0d401d411e3efe8defb3
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 19:23:59 +0300

    Create EventNotification.cs

---
+++ src/Libraries/Doctorla.Domain/Common/Events/EventNotification.cs
@@ -0,0 +1,12 @@
+using Doctorla.Shared.Events;
+using MediatR;
+
+namespace Doctorla.Application.Common.Events;
+
+public class EventNotification<TEvent> : INotification
+    where TEvent : IEvent
+{
+    public EventNotification(TEvent @event) => Event = @event;
+
+    public TEvent Event { get; }
+}
