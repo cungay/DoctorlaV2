02a012dad26032130158d5096bb31798245d52de
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 17:00:29 +0300

    Create DomainEvent.cs

---
+++ src/Libraries/Doctorla.Domain/Common/Contracts/DomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using Doctorla.Shared.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctorla.Domain.Common.Contracts
+{
+    public abstract class DomainEvent : IEvent
+    {
+        public DateTime TriggeredOn { get; protected set; } = DateTime.UtcNow;
+    }
+}
