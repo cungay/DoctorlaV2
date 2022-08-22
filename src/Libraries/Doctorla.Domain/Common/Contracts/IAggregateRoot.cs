81939bda95329986124be23fba589abd258ffb8f
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 22:27:08 +0300

    Update IAggregateRoot.cs

--- src/Libraries/Doctorla.Domain/Common/Contracts/IAggregateRoot.cs
+++ src/Libraries/Doctorla.Domain/Common/Contracts/IAggregateRoot.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Doctorla.Domain.Common.Contracts;
 
-namespace Doctorla.Domain.Common.Contracts
+// Apply this marker interface only to aggregate root entities
+// Repositories will only work with aggregate roots, not their children
+public interface IAggregateRoot : IEntity
 {
-    // Apply this marker interface only to aggregate root entities
-    // Repositories will only work with aggregate roots, not their children
-    public interface IAggregateRoot : IEntity
-    {
-    }
 }
