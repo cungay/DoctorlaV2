e609dc1a8eeccb49bd1b5dc58bb72c9cdea62b99
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 17:00:21 +0300

    Create DbProviderKeys.cs

---
+++ src/Libraries/Doctorla.Infrastructure/Common/DbProviderKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctorla.Infrastructure.Common
+{
+    internal class DbProviderKeys
+    {
+        public const string Npgsql = "postgresql";
+        public const string SqlServer = "mssql";
+        public const string MySql = "mysql";
+        public const string SqLite = "sqlite";
+    }
+}
