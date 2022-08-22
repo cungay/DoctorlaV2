c93bc1ef682049e8077c9ffa6f20cf69cdc117c8
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 19:22:38 +0300

    Create GlobalUsings.cs

---
+++ src/Libraries/Doctorla.Application/GlobalUsings.cs
@@ -0,0 +1,16 @@
+global using FluentValidation;
+global using Doctorla.Application.Common.Events;
+//global using Doctorla.Application.Common.Exceptions;
+//global using Doctorla.Application.Common.FileStorage;
+//global using Doctorla.Application.Common.Interfaces;
+//global using Doctorla.Application.Common.Models;
+global using Doctorla.Application.Common.Persistence;
+//global using Doctorla.Application.Common.Specification;
+//global using Doctorla.Application.Common.Validation;
+//global using Doctorla.Domain.Catalog;
+global using Doctorla.Domain.Common;
+global using Doctorla.Domain.Common.Contracts;
+//global using Doctorla.Shared.Notifications;
+global using MediatR;
+global using Microsoft.Extensions.Localization;
+global using Microsoft.Extensions.Logging;
