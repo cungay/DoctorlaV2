001df0587942d36ff12c8cbeda02d150c43adb80
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-22 17:00:24 +0300

    Create IEntity.cs

---
+++ src/Libraries/Doctorla.Domain/Common/Contracts/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctorla.Domain.Common.Contracts
+{
+    public interface IEntity
+    {
+        List<DomainEvent> DomainEvents { get; }
+    }
+
+    public interface IEntity<TId> : IEntity
+    {
+        TId Id { get; }
+    }
+}
