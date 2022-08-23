03bbec3a6ceba6e950dd4b73fc77e343d4f5d0da
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-23 23:29:33 +0300

    Update MustHavePermissionAttribute.cs

--- src/Libraries/Doctorla.Infrastructure/Auth/Permissions/MustHavePermissionAttribute.cs
+++ src/Libraries/Doctorla.Infrastructure/Auth/Permissions/MustHavePermissionAttribute.cs
@@ -6,5 +6,5 @@ namespace Doctorla.Infrastructure.Auth.Permissions;
 public class MustHavePermissionAttribute : AuthorizeAttribute
 {
     public MustHavePermissionAttribute(string action, string resource) =>
-        Policy = FSHPermission.NameFor(action, resource);
+        Policy = DocPermission.NameFor(action, resource);
 }
