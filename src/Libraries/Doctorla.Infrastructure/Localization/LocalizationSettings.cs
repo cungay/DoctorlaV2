d87bcbcd406ddccf56a82ea173b2024e947f9234
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 00:22:25 +0300

    Update LocalizationSettings.cs

--- src/Libraries/Doctorla.Infrastructure/Localization/LocalizationSettings.cs
+++ src/Libraries/Doctorla.Infrastructure/Localization/LocalizationSettings.cs
@@ -3,8 +3,12 @@
 public class LocalizationSettings
 {
     public bool? EnableLocalization { get; set; }
+
     public string? ResourcesPath { get; set; }
+    
     public string[]? SupportedCultures { get; set; }
+    
     public string? DefaultRequestCulture { get; set; }
+    
     public bool? FallbackToParent { get; set; }
 }
