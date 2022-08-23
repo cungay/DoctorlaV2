b9037ac868163af879cde3100083600a37cc1f21
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 02:55:16 +0300

    Update CreateTenantRequest.cs

--- src/Libraries/Doctorla.Application/Multitenancy/CreateTenantRequest.cs
+++ src/Libraries/Doctorla.Application/Multitenancy/CreateTenantRequest.cs
@@ -3,18 +3,22 @@ namespace Doctorla.Application.Multitenancy;
 public class CreateTenantRequest : IRequest<string>
 {
     public string Id { get; set; } = default!;
+
     public string Name { get; set; } = default!;
+    
     public string? ConnectionString { get; set; }
+    
     public string AdminEmail { get; set; } = default!;
+    
     public string? Issuer { get; set; }
 }
 
 public class CreateTenantRequestHandler : IRequestHandler<CreateTenantRequest, string>
 {
-    private readonly ITenantService _tenantService;
+    private readonly ITenantService tenantService = null;
 
-    public CreateTenantRequestHandler(ITenantService tenantService) => _tenantService = tenantService;
+    public CreateTenantRequestHandler(ITenantService tenantService) => this.tenantService = tenantService;
 
     public Task<string> Handle(CreateTenantRequest request, CancellationToken cancellationToken) =>
-        _tenantService.CreateAsync(request, cancellationToken);
+        this.tenantService.CreateAsync(request, cancellationToken);
 }
