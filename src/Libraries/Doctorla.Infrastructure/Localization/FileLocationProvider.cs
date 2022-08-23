308b89cdfff9e285dd570b1678345fb46b1ce8d5
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 00:23:02 +0300

    Update FileLocationProvider.cs

--- src/Libraries/Doctorla.Infrastructure/Localization/FileLocationProvider.cs
+++ src/Libraries/Doctorla.Infrastructure/Localization/FileLocationProvider.cs
@@ -12,20 +12,20 @@ namespace Doctorla.Infrastructure.Localization;
 /// </summary>
 public class FileLocationProvider : ILocalizationFileLocationProvider
 {
-    private readonly IFileProvider _fileProvider;
-    private readonly string _resourcesContainer;
+    private readonly IFileProvider fileProvider = null;
+    private readonly string resourcesContainer = null;
 
     public FileLocationProvider(IHostEnvironment hostingEnvironment, IOptions<LocalizationOptions> localizationOptions)
     {
-        _fileProvider = hostingEnvironment.ContentRootFileProvider;
-        _resourcesContainer = localizationOptions.Value.ResourcesPath;
+        this.fileProvider = hostingEnvironment.ContentRootFileProvider;
+        this.resourcesContainer = localizationOptions.Value.ResourcesPath;
     }
 
     public IEnumerable<IFileInfo> GetLocations(string cultureName)
     {
         // Loads all *.po files from the culture folder under the Resource Path.
         // for example, src\Host\Localization\en-US\FSH.Exceptions.po
-        foreach (var file in _fileProvider.GetDirectoryContents(PathExtensions.Combine(_resourcesContainer, cultureName)))
+        foreach (var file in fileProvider.GetDirectoryContents(PathExtensions.Combine(_resourcesContainer, cultureName)))
         {
             yield return file;
         }
