ad9b27199d0b2eec1e35f810de849d97db115f2a
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 00:08:33 +0300

    Update SwaggerHeaderAttribute.cs

--- src/Libraries/Doctorla.Infrastructure/OpenApi/SwaggerHeaderAttribute.cs
+++ src/Libraries/Doctorla.Infrastructure/OpenApi/SwaggerHeaderAttribute.cs
@@ -4,8 +4,11 @@
 public class SwaggerHeaderAttribute : Attribute
 {
     public string HeaderName { get; }
+
     public string? Description { get; }
+    
     public string? DefaultValue { get; }
+    
     public bool IsRequired { get; }
 
     public SwaggerHeaderAttribute(string headerName, string? description = null, string? defaultValue = null, bool isRequired = false)
