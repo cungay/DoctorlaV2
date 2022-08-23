da702f3170e5375544b4fe2943e5792ea1ff332e
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 02:35:43 +0300

    Update CacheKeyService.cs

--- src/Libraries/Doctorla.Infrastructure/Caching/CacheKeyService.cs
+++ src/Libraries/Doctorla.Infrastructure/Caching/CacheKeyService.cs
@@ -5,14 +5,14 @@ namespace Doctorla.Infrastructure.Caching;
 
 public class CacheKeyService : ICacheKeyService
 {
-    private readonly ITenantInfo? _currentTenant;
+    private readonly ITenantInfo? currentTenant = null;
 
-    public CacheKeyService(ITenantInfo currentTenant) => _currentTenant = currentTenant;
+    public CacheKeyService(ITenantInfo currentTenant) => this.currentTenant = currentTenant;
 
     public string GetCacheKey(string name, object id, bool includeTenantId = true)
     {
         string tenantId = includeTenantId
-            ? _currentTenant?.Id ?? throw new InvalidOperationException("GetCacheKey: includeTenantId set to true and no ITenantInfo available.")
+            ? currentTenant?.Id ?? throw new InvalidOperationException("GetCacheKey: includeTenantId set to true and no ITenantInfo available.")
             : "GLOBAL";
         return $"{tenantId}-{name}-{id}";
     }
