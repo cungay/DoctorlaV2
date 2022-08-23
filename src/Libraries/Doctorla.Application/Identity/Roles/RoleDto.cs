3821122b410372dd36b4f07aa7885e9b32bb92a8
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 02:59:21 +0300

    Update RoleDto.cs

--- src/Libraries/Doctorla.Application/Identity/Roles/RoleDto.cs
+++ src/Libraries/Doctorla.Application/Identity/Roles/RoleDto.cs
@@ -3,7 +3,10 @@ namespace Doctorla.Application.Identity.Roles;
 public class RoleDto
 {
     public string Id { get; set; } = default!;
+
     public string Name { get; set; } = default!;
+    
     public string? Description { get; set; }
+    
     public List<string>? Permissions { get; set; }
 }
