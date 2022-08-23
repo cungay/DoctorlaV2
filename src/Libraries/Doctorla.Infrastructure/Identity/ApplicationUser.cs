ac34cc82506434423d673f5ce538a61ee6d55f70
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 00:24:22 +0300

    Update ApplicationUser.cs

--- src/Libraries/Doctorla.Infrastructure/Identity/ApplicationUser.cs
+++ src/Libraries/Doctorla.Infrastructure/Identity/ApplicationUser.cs
@@ -5,10 +5,15 @@ namespace Doctorla.Infrastructure.Identity;
 public class ApplicationUser : IdentityUser
 {
     public string? FirstName { get; set; }
+
     public string? LastName { get; set; }
+
     public string? ImageUrl { get; set; }
+
     public bool IsActive { get; set; }
+
     public string? RefreshToken { get; set; }
+
     public DateTime RefreshTokenExpiryTime { get; set; }
 
     public string? ObjectId { get; set; }
