8ef9b8f7ed93dc2bdeb859109d681511fe8e9844
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 01:42:22 +0300

    Update UserService.cs

--- src/Libraries/Doctorla.Infrastructure/Identity/UserService.cs
+++ src/Libraries/Doctorla.Infrastructure/Identity/UserService.cs
@@ -21,20 +21,20 @@ namespace Doctorla.Infrastructure.Identity;
 
 internal partial class UserService : IUserService
 {
-    private readonly SignInManager<ApplicationUser> _signInManager;
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly RoleManager<ApplicationRole> _roleManager;
-    private readonly ApplicationDbContext _db;
-    private readonly IStringLocalizer _t;
-    private readonly IJobService _jobService;
-    private readonly IMailService _mailService;
-    private readonly SecuritySettings _securitySettings;
-    private readonly IEmailTemplateService _templateService;
-    private readonly IFileStorageService _fileStorage;
-    private readonly IEventPublisher _events;
-    private readonly ICacheService _cache;
-    private readonly ICacheKeyService _cacheKeys;
-    private readonly ITenantInfo _currentTenant;
+    private readonly SignInManager<ApplicationUser> signInManager = null;
+    private readonly UserManager<ApplicationUser> userManager = null;
+    private readonly RoleManager<ApplicationRole> roleManager = null;
+    private readonly ApplicationDbContext db = null;
+    private readonly IStringLocalizer localizer = null;
+    private readonly IJobService jobService = null;
+    private readonly IMailService mailService = null;
+    private readonly SecuritySettings securitySettings = null;
+    private readonly IEmailTemplateService templateService = null;
+    private readonly IFileStorageService fileStorage = null;
+    private readonly IEventPublisher events = null;
+    private readonly ICacheService cache = null;
+    private readonly ICacheKeyService cacheKeys = null;
+    private readonly ITenantInfo currentTenant = null;
 
     public UserService(
         SignInManager<ApplicationUser> signInManager,
@@ -52,31 +52,31 @@ internal partial class UserService : IUserService
         ITenantInfo currentTenant,
         IOptions<SecuritySettings> securitySettings)
     {
-        _signInManager = signInManager;
-        _userManager = userManager;
-        _roleManager = roleManager;
-        _db = db;
-        _t = localizer;
-        _jobService = jobService;
-        _mailService = mailService;
-        _templateService = templateService;
-        _fileStorage = fileStorage;
-        _events = events;
-        _cache = cache;
-        _cacheKeys = cacheKeys;
-        _currentTenant = currentTenant;
-        _securitySettings = securitySettings.Value;
+        this.signInManager = signInManager;
+        this.userManager = userManager;
+        this.roleManager = roleManager;
+        this.db = db;
+        this.localizer = localizer;
+        this.jobService = jobService;
+        this.mailService = mailService;
+        this.templateService = templateService;
+        this.fileStorage = fileStorage;
+        this.events = events;
+        this.cache = cache;
+        this.cacheKeys = cacheKeys;
+        this.currentTenant = currentTenant;
+        this.securitySettings = securitySettings.Value;
     }
 
     public async Task<PaginationResponse<UserDetailsDto>> SearchAsync(UserListFilter filter, CancellationToken cancellationToken)
     {
         //var spec = new EntitiesByPaginationFilterSpec<ApplicationUser>(filter);
 
-        var users = await _userManager.Users
+        var users = await userManager.Users
             //.WithSpecification(spec)
             .ProjectToType<UserDetailsDto>()
             .ToListAsync(cancellationToken);
-        int count = await _userManager.Users
+        int count = await userManager.Users
             .CountAsync(cancellationToken);
 
         return new PaginationResponse<UserDetailsDto>(users, count, filter.PageNumber, filter.PageSize);
@@ -85,41 +85,41 @@ internal partial class UserService : IUserService
     public async Task<bool> ExistsWithNameAsync(string name)
     {
         EnsureValidTenant();
-        return await _userManager.FindByNameAsync(name) is not null;
+        return await userManager.FindByNameAsync(name) is not null;
     }
 
     public async Task<bool> ExistsWithEmailAsync(string email, string? exceptId = null)
     {
         EnsureValidTenant();
-        return await _userManager.FindByEmailAsync(email.Normalize()) is ApplicationUser user && user.Id != exceptId;
+        return await userManager.FindByEmailAsync(email.Normalize()) is ApplicationUser user && user.Id != exceptId;
     }
 
     public async Task<bool> ExistsWithPhoneNumberAsync(string phoneNumber, string? exceptId = null)
     {
         EnsureValidTenant();
-        return await _userManager.Users.FirstOrDefaultAsync(x => x.PhoneNumber == phoneNumber) is ApplicationUser user && user.Id != exceptId;
+        return await userManager.Users.FirstOrDefaultAsync(x => x.PhoneNumber == phoneNumber) is ApplicationUser user && user.Id != exceptId;
     }
 
     private void EnsureValidTenant()
     {
-        if (string.IsNullOrWhiteSpace(_currentTenant?.Id))
+        if (string.IsNullOrWhiteSpace(currentTenant?.Id))
         {
-            throw new UnauthorizedException(_t["Invalid Tenant."]);
+            throw new UnauthorizedException(localizer["Invalid Tenant."]);
         }
     }
 
     public async Task<List<UserDetailsDto>> GetListAsync(CancellationToken cancellationToken) =>
-        (await _userManager.Users
+        (await userManager.Users
                 .AsNoTracking()
                 .ToListAsync(cancellationToken))
             .Adapt<List<UserDetailsDto>>();
 
     public Task<int> GetCountAsync(CancellationToken cancellationToken) =>
-        _userManager.Users.AsNoTracking().CountAsync(cancellationToken);
+        userManager.Users.AsNoTracking().CountAsync(cancellationToken);
 
     public async Task<UserDetailsDto> GetAsync(string userId, CancellationToken cancellationToken)
     {
-        var user = await _userManager.Users
+        var user = await userManager.Users
             .AsNoTracking()
             .Where(u => u.Id == userId)
             .FirstOrDefaultAsync(cancellationToken);
@@ -131,11 +131,11 @@ internal partial class UserService : IUserService
 
     public async Task ToggleStatusAsync(ToggleUserStatusRequest request, CancellationToken cancellationToken)
     {
-        var user = await _userManager.Users.Where(u => u.Id == request.UserId).FirstOrDefaultAsync(cancellationToken);
+        var user = await userManager.Users.Where(u => u.Id == request.UserId).FirstOrDefaultAsync(cancellationToken);
 
         _ = user ?? throw new NotFoundException(_t["User Not Found."]);
 
-        bool isAdmin = await _userManager.IsInRoleAsync(user, DocRoles.Admin);
+        bool isAdmin = await userManager.IsInRoleAsync(user, DocRoles.Admin);
         if (isAdmin)
         {
             throw new ConflictException(_t["Administrators Profile's Status cannot be toggled"]);
@@ -143,8 +143,8 @@ internal partial class UserService : IUserService
 
         user.IsActive = request.ActivateUser;
 
-        await _userManager.UpdateAsync(user);
+        await userManager.UpdateAsync(user);
 
-        await _events.PublishAsync(new ApplicationUserUpdatedEvent(user.Id));
+        await events.PublishAsync(new ApplicationUserUpdatedEvent(user.Id));
     }
 }
