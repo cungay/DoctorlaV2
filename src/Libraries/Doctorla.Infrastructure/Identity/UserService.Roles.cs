86fd9830234e41468acd42150f7c655ef428d52f
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 01:42:18 +0300

    Update UserService.Roles.cs

--- src/Libraries/Doctorla.Infrastructure/Identity/UserService.Roles.cs
+++ src/Libraries/Doctorla.Infrastructure/Identity/UserService.Roles.cs
@@ -13,8 +13,8 @@ internal partial class UserService
     {
         var userRoles = new List<UserRoleDto>();
 
-        var user = await _userManager.FindByIdAsync(userId);
-        var roles = await _roleManager.Roles.AsNoTracking().ToListAsync(cancellationToken);
+        var user = await userManager.FindByIdAsync(userId);
+        var roles = await roleManager.Roles.AsNoTracking().ToListAsync(cancellationToken);
         foreach (var role in roles)
         {
             userRoles.Add(new UserRoleDto
@@ -22,7 +22,7 @@ internal partial class UserService
                 RoleId = role.Id,
                 RoleName = role.Name,
                 Description = role.Description,
-                Enabled = await _userManager.IsInRoleAsync(user, role.Name)
+                Enabled = await userManager.IsInRoleAsync(user, role.Name)
             });
         }
 
@@ -33,53 +33,53 @@ internal partial class UserService
     {
         ArgumentNullException.ThrowIfNull(request, nameof(request));
 
-        var user = await _userManager.Users.Where(u => u.Id == userId).FirstOrDefaultAsync(cancellationToken);
+        var user = await userManager.Users.Where(u => u.Id == userId).FirstOrDefaultAsync(cancellationToken);
 
-        _ = user ?? throw new NotFoundException(_t["User Not Found."]);
+        _ = user ?? throw new NotFoundException(localizer["User Not Found."]);
 
         // Check if the user is an admin for which the admin role is getting disabled
-        if (await _userManager.IsInRoleAsync(user, DocRoles.Admin)
+        if (await userManager.IsInRoleAsync(user, DocRoles.Admin)
             && request.UserRoles.Any(a => !a.Enabled && a.RoleName == DocRoles.Admin))
         {
             // Get count of users in Admin Role
-            int adminCount = (await _userManager.GetUsersInRoleAsync(DocRoles.Admin)).Count;
+            int adminCount = (await userManager.GetUsersInRoleAsync(DocRoles.Admin)).Count;
 
             // Check if user is not Root Tenant Admin
             // Edge Case : there are chances for other tenants to have users with the same email as that of Root Tenant Admin. Probably can add a check while User Registration
             if (user.Email == MultitenancyConstants.Root.EmailAddress)
             {
-                if (_currentTenant.Id == MultitenancyConstants.Root.Id)
+                if (currentTenant.Id == MultitenancyConstants.Root.Id)
                 {
-                    throw new ConflictException(_t["Cannot Remove Admin Role From Root Tenant Admin."]);
+                    throw new ConflictException(localizer["Cannot Remove Admin Role From Root Tenant Admin."]);
                 }
             }
             else if (adminCount <= 2)
             {
-                throw new ConflictException(_t["Tenant should have at least 2 Admins."]);
+                throw new ConflictException(localizer["Tenant should have at least 2 Admins."]);
             }
         }
 
         foreach (var userRole in request.UserRoles)
         {
             // Check if Role Exists
-            if (await _roleManager.FindByNameAsync(userRole.RoleName) is not null)
+            if (await roleManager.FindByNameAsync(userRole.RoleName) is not null)
             {
                 if (userRole.Enabled)
                 {
-                    if (!await _userManager.IsInRoleAsync(user, userRole.RoleName))
+                    if (!await userManager.IsInRoleAsync(user, userRole.RoleName))
                     {
-                        await _userManager.AddToRoleAsync(user, userRole.RoleName);
+                        await userManager.AddToRoleAsync(user, userRole.RoleName);
                     }
                 }
                 else
                 {
-                    await _userManager.RemoveFromRoleAsync(user, userRole.RoleName);
+                    await userManager.RemoveFromRoleAsync(user, userRole.RoleName);
                 }
             }
         }
 
-        await _events.PublishAsync(new ApplicationUserUpdatedEvent(user.Id, true));
+        await events.PublishAsync(new ApplicationUserUpdatedEvent(user.Id, true));
 
-        return _t["User Roles Updated Successfully."];
+        return localizer["User Roles Updated Successfully."];
     }
 }
