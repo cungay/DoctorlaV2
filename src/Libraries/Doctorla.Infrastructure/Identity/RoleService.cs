128b6520aae4e7d4a994d9846634046c2267fb7c
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 00:28:16 +0300

    Update RoleService.cs

--- src/Libraries/Doctorla.Infrastructure/Identity/RoleService.cs
+++ src/Libraries/Doctorla.Infrastructure/Identity/RoleService.cs
@@ -16,13 +16,13 @@ namespace Doctorla.Infrastructure.Identity;
 
 internal class RoleService : IRoleService
 {
-    private readonly RoleManager<ApplicationRole> _roleManager;
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly ApplicationDbContext _db;
-    private readonly IStringLocalizer _t;
-    private readonly ICurrentUser _currentUser;
-    private readonly ITenantInfo _currentTenant;
-    private readonly IEventPublisher _events;
+    private readonly RoleManager<ApplicationRole> roleManager = null;
+    private readonly UserManager<ApplicationUser> userManager = null;
+    private readonly ApplicationDbContext db = null;
+    private readonly IStringLocalizer localizer = null;
+    private readonly ICurrentUser  currentUser = null;
+    private readonly ITenantInfo currentTenant = null;
+    private readonly IEventPublisher events = null;
 
     public RoleService(
         RoleManager<ApplicationRole> roleManager,
@@ -33,37 +33,37 @@ internal class RoleService : IRoleService
         ITenantInfo currentTenant,
         IEventPublisher events)
     {
-        _roleManager = roleManager;
-        _userManager = userManager;
-        _db = db;
-        _t = localizer;
-        _currentUser = currentUser;
-        _currentTenant = currentTenant;
-        _events = events;
+        this.roleManager = roleManager;
+        this.userManager = userManager;
+        this.db = db;
+        this.localizer = localizer;
+        this.currentUser = currentUser;
+        this.currentTenant = currentTenant;
+        this.events = events;
     }
 
     public async Task<List<RoleDto>> GetListAsync(CancellationToken cancellationToken) =>
-        (await _roleManager.Roles.ToListAsync(cancellationToken))
+        (await roleManager.Roles.ToListAsync(cancellationToken))
             .Adapt<List<RoleDto>>();
 
     public async Task<int> GetCountAsync(CancellationToken cancellationToken) =>
-        await _roleManager.Roles.CountAsync(cancellationToken);
+        await roleManager.Roles.CountAsync(cancellationToken);
 
     public async Task<bool> ExistsAsync(string roleName, string? excludeId) =>
-        await _roleManager.FindByNameAsync(roleName)
+        await roleManager.FindByNameAsync(roleName)
             is ApplicationRole existingRole
             && existingRole.Id != excludeId;
 
     public async Task<RoleDto> GetByIdAsync(string id) =>
-        await _db.Roles.SingleOrDefaultAsync(x => x.Id == id) is { } role
+        await db.Roles.SingleOrDefaultAsync(x => x.Id == id) is { } role
             ? role.Adapt<RoleDto>()
-            : throw new NotFoundException(_t["Role Not Found"]);
+            : throw new NotFoundException(localizer["Role Not Found"]);
 
     public async Task<RoleDto> GetByIdWithPermissionsAsync(string roleId, CancellationToken cancellationToken)
     {
         var role = await GetByIdAsync(roleId);
 
-        role.Permissions = await _db.RoleClaims
+        role.Permissions = await db.RoleClaims
             .Where(c => c.RoleId == roleId && c.ClaimType == DocClaims.Permission)
             .Select(c => c.ClaimValue)
             .ToListAsync(cancellationToken);
@@ -77,69 +77,69 @@ internal class RoleService : IRoleService
         {
             // Create a new role.
             var role = new ApplicationRole(request.Name, request.Description);
-            var result = await _roleManager.CreateAsync(role);
+            var result = await roleManager.CreateAsync(role);
 
             if (!result.Succeeded)
             {
-                throw new InternalServerException(_t["Register role failed"], result.GetErrors(_t));
+                throw new InternalServerException(localizer["Register role failed"], result.GetErrors(localizer));
             }
 
-            await _events.PublishAsync(new ApplicationRoleCreatedEvent(role.Id, role.Name));
+            await events.PublishAsync(new ApplicationRoleCreatedEvent(role.Id, role.Name));
 
-            return string.Format(_t["Role {0} Created."], request.Name);
+            return string.Format(localizer["Role {0} Created."], request.Name);
         }
         else
         {
             // Update an existing role.
-            var role = await _roleManager.FindByIdAsync(request.Id);
+            var role = await roleManager.FindByIdAsync(request.Id);
 
-            _ = role ?? throw new NotFoundException(_t["Role Not Found"]);
+            _ = role ?? throw new NotFoundException(localizer["Role Not Found"]);
 
             if (DocRoles.IsDefault(role.Name))
             {
-                throw new ConflictException(string.Format(_t["Not allowed to modify {0} Role."], role.Name));
+                throw new ConflictException(string.Format(localizer["Not allowed to modify {0} Role."], role.Name));
             }
 
             role.Name = request.Name;
             role.NormalizedName = request.Name.ToUpperInvariant();
             role.Description = request.Description;
-            var result = await _roleManager.UpdateAsync(role);
+            var result = await roleManager.UpdateAsync(role);
 
             if (!result.Succeeded)
             {
-                throw new InternalServerException(_t["Update role failed"], result.GetErrors(_t));
+                throw new InternalServerException(localizer["Update role failed"], result.GetErrors(localizer));
             }
 
-            await _events.PublishAsync(new ApplicationRoleUpdatedEvent(role.Id, role.Name));
+            await events.PublishAsync(new ApplicationRoleUpdatedEvent(role.Id, role.Name));
 
-            return string.Format(_t["Role {0} Updated."], role.Name);
+            return string.Format(localizer["Role {0} Updated."], role.Name);
         }
     }
 
     public async Task<string> UpdatePermissionsAsync(UpdateRolePermissionsRequest request, CancellationToken cancellationToken)
     {
-        var role = await _roleManager.FindByIdAsync(request.RoleId);
-        _ = role ?? throw new NotFoundException(_t["Role Not Found"]);
+        var role = await roleManager.FindByIdAsync(request.RoleId);
+        _ = role ?? throw new NotFoundException(localizer["Role Not Found"]);
         if (role.Name == DocRoles.Admin)
         {
-            throw new ConflictException(_t["Not allowed to modify Permissions for this Role."]);
+            throw new ConflictException(localizer["Not allowed to modify Permissions for this Role."]);
         }
 
-        if (_currentTenant.Id != MultitenancyConstants.Root.Id)
+        if (currentTenant.Id != MultitenancyConstants.Root.Id)
         {
             // Remove Root Permissions if the Role is not created for Root Tenant.
             request.Permissions.RemoveAll(u => u.StartsWith("Permissions.Root."));
         }
 
-        var currentClaims = await _roleManager.GetClaimsAsync(role);
+        var currentClaims = await roleManager.GetClaimsAsync(role);
 
         // Remove permissions that were previously selected
         foreach (var claim in currentClaims.Where(c => !request.Permissions.Any(p => p == c.Value)))
         {
-            var removeResult = await _roleManager.RemoveClaimAsync(role, claim);
+            var removeResult = await roleManager.RemoveClaimAsync(role, claim);
             if (!removeResult.Succeeded)
             {
-                throw new InternalServerException(_t["Update permissions failed."], removeResult.GetErrors(_t));
+                throw new InternalServerException(localizer["Update permissions failed."], removeResult.GetErrors(localizer));
             }
         }
 
@@ -148,42 +148,42 @@ internal class RoleService : IRoleService
         {
             if (!string.IsNullOrEmpty(permission))
             {
-                _db.RoleClaims.Add(new ApplicationRoleClaim
+                db.RoleClaims.Add(new ApplicationRoleClaim
                 {
                     RoleId = role.Id,
                     ClaimType = DocClaims.Permission,
                     ClaimValue = permission,
-                    CreatedBy = _currentUser.GetUserId().ToString()
+                    CreatedBy = currentUser.GetUserId().ToString()
                 });
-                await _db.SaveChangesAsync(cancellationToken);
+                await db.SaveChangesAsync(cancellationToken);
             }
         }
 
-        await _events.PublishAsync(new ApplicationRoleUpdatedEvent(role.Id, role.Name, true));
+        await events.PublishAsync(new ApplicationRoleUpdatedEvent(role.Id, role.Name, true));
 
-        return _t["Permissions Updated."];
+        return localizer["Permissions Updated."];
     }
 
     public async Task<string> DeleteAsync(string id)
     {
-        var role = await _roleManager.FindByIdAsync(id);
+        var role = await roleManager.FindByIdAsync(id);
 
-        _ = role ?? throw new NotFoundException(_t["Role Not Found"]);
+        _ = role ?? throw new NotFoundException(localizer["Role Not Found"]);
 
         if (DocRoles.IsDefault(role.Name))
         {
-            throw new ConflictException(string.Format(_t["Not allowed to delete {0} Role."], role.Name));
+            throw new ConflictException(string.Format(localizer["Not allowed to delete {0} Role."], role.Name));
         }
 
-        if ((await _userManager.GetUsersInRoleAsync(role.Name)).Count > 0)
+        if ((await userManager.GetUsersInRoleAsync(role.Name)).Count > 0)
         {
-            throw new ConflictException(string.Format(_t["Not allowed to delete {0} Role as it is being used."], role.Name));
+            throw new ConflictException(string.Format(localizer["Not allowed to delete {0} Role as it is being used."], role.Name));
         }
 
-        await _roleManager.DeleteAsync(role);
+        await roleManager.DeleteAsync(role);
 
-        await _events.PublishAsync(new ApplicationRoleDeletedEvent(role.Id, role.Name));
+        await events.PublishAsync(new ApplicationRoleDeletedEvent(role.Id, role.Name));
 
-        return string.Format(_t["Role {0} Deleted."], role.Name);
+        return string.Format(localizer["Role {0} Deleted."], role.Name);
     }
 }
