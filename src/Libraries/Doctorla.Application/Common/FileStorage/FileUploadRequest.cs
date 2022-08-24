94328b9b949e6b81c780d660b805ae860aa34de7
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 03:06:52 +0300

    Update FileUploadRequest.cs

--- src/Libraries/Doctorla.Application/Common/FileStorage/FileUploadRequest.cs
+++ src/Libraries/Doctorla.Application/Common/FileStorage/FileUploadRequest.cs
@@ -3,7 +3,9 @@ namespace Doctorla.Application.Common.FileStorage;
 public class FileUploadRequest
 {
     public string Name { get; set; } = default!;
+
     public string Extension { get; set; } = default!;
+    
     public string Data { get; set; } = default!;
 }
 
