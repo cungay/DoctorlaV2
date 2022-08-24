4f5208a141bf70ee25b8140ac99e8e487b2b88d3
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-24 22:24:49 +0300

    Update CurrentUser.cs

--- src/Libraries/Doctorla.Infrastructure/Auth/CurrentUser.cs
+++ src/Libraries/Doctorla.Infrastructure/Auth/CurrentUser.cs
@@ -5,54 +5,54 @@ namespace Doctorla.Infrastructure.Auth;
 
 public class CurrentUser : ICurrentUser, ICurrentUserInitializer
 {
-    private ClaimsPrincipal? user = null;
+    private ClaimsPrincipal? currentUser = null;
 
-    public string? Name => user?.Identity?.Name;
+    public string? Name => currentUser?.Identity?.Name;
 
-    private Guid cachedUserId = Guid.Empty;
+    private Guid currentUserId = Guid.Empty;
 
     public Guid GetUserId() =>
         IsAuthenticated()
-            ? Guid.Parse(user?.GetUserId() ?? Guid.Empty.ToString())
-            : cachedUserId;
+            ? Guid.Parse(currentUser?.GetUserId() ?? Guid.Empty.ToString())
+            : currentUserId;
 
     public string? GetUserEmail() =>
         IsAuthenticated()
-            ? user!.GetEmail()
+            ? currentUser!.GetEmail()
             : string.Empty;
 
     public bool IsAuthenticated() =>
-        user?.Identity?.IsAuthenticated is true;
+        currentUser?.Identity?.IsAuthenticated is true;
 
     public bool IsInRole(string role) =>
-        user?.IsInRole(role) is true;
+        currentUser?.IsInRole(role) is true;
 
     public IEnumerable<Claim>? GetUserClaims() =>
-        user?.Claims;
+        currentUser?.Claims;
 
     public string? GetTenant() =>
-        IsAuthenticated() ? user?.GetTenant() : string.Empty;
+        IsAuthenticated() ? currentUser?.GetTenant() : string.Empty;
 
     public void SetCurrentUser(ClaimsPrincipal user)
     {
-        if (user != null)
+        if (currentUser != null)
         {
             throw new Exception("Method reserved for in-scope initialization");
         }
 
-        this.user = user;
+        currentUser = user;
     }
 
     public void SetCurrentUserId(string userId)
     {
-        if (cachedUserId != Guid.Empty)
+        if (currentUserId != Guid.Empty)
         {
             throw new Exception("Method reserved for in-scope initialization");
         }
 
         if (!string.IsNullOrEmpty(userId))
         {
-            cachedUserId = Guid.Parse(userId);
+            currentUserId = Guid.Parse(userId);
         }
     }
 }
