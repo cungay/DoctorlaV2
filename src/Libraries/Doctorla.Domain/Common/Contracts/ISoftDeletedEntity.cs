5969724123ee0d03b930747a5137ea5914c678c2
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-25 18:13:08 +0300

    Update ISoftDeletedEntity.cs

--- src/Libraries/Doctorla.Domain/Common/Contracts/ISoftDeletedEntity.cs
+++ src/Libraries/Doctorla.Domain/Common/Contracts/ISoftDeletedEntity.cs
@@ -2,6 +2,15 @@ namespace Doctorla.Domain.Common.Contracts;
 
 public interface ISoftDeletedEntity
 {
+    bool Deleted
+    {
+        get
+        {
+            var deleted = (DeletedOn.HasValue && DeletedOn.Value > DateTime.MinValue) && (DeletedBy.HasValue && DeletedBy.Value != Guid.Empty);
+            return deleted;
+        }
+    }
+
     DateTime? DeletedOn { get; set; }
 
     Guid? DeletedBy { get; set; }
