5d0053113eec844a6aa18fdca51af428e6ac6177
İhsan Çungay <ihsan_cungay@hotmail.com>
2022-08-25 14:31:51 +0300

    Update GlobalUsings.cs

--- src/Presentation/Doctorla.Host/GlobalUsings.cs
+++ src/Presentation/Doctorla.Host/GlobalUsings.cs
@@ -1,4 +1,4 @@
-global using Doctorla.Application.Common.Models;
+global using Doctorla.Application.Models;
 global using Doctorla.Infrastructure.Auth.Permissions;
 global using Doctorla.Infrastructure.OpenApi;
 global using Doctorla.Shared.Authorization;
